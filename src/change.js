90981e95c98490509a51a10329805569fea7874f
nhicu <55456980+nhicu@users.noreply.github.com>
2019-10-31 13:19:59 -0400

    lol

--- src/change.js
+++ src/change.js
@@ -5,6 +5,11 @@ const MAX = 0.99;
 
 const amount = Number(readlineSync.question("\nEnter a dollar amount: "));
 
+let Q = 0.25
+let D = 0.10
+let N = 0.05
+let P = 0.01
+
 if (Number.isNaN(amount)) {
     // print your error message here
 } else if (amount < MIN || amount > MAX) {
