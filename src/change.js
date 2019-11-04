d48a18f81b7ba32c2ec9d8c6f831b85bcb19de55
nhicu <55456980+nhicu@users.noreply.github.com>
2019-11-04 12:46:23 -0500

    umh

--- src/change.js
+++ src/change.js
@@ -5,15 +5,25 @@ const MAX = 0.99;
 
 const amount = Number(readlineSync.question("\nEnter a dollar amount: "));
 
-let Q = 0.25
-let D = 0.10
-let N = 0.05
-let P = 0.01
+let Q = 0.25;
+let D = 0.10;
+let N = 0.05;
+let P = 0.01;
+
 
 if (Number.isNaN(amount)) {
-    // print your error message here
+  console.log("\nInvalid");
 } else if (amount < MIN || amount > MAX) {
-    // print your error message here
+  console.log("invalid");
 } else {
+
+  let Quarters = Math.floor(amount/Q);
+  let amount1 = amount % Q;
+  let Dimes = Math.floor(amount1/D);
+  let amount2 = amount1 % D;
+  let Nickels = Math.floor (amount2/N);
+  let amount3 = amount2 % N;
+  let Pennies = Math.ceil(amount3 / P);
     // write your code for making change here
+  console.log("\n" + Quarters + " quarters, " + Dimes + " dimes, " + Nickels + " nickels, and " + Pennies + " pennies.");
 }
