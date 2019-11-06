e3a1d7a03abe89e3fe49a4fdf9d01d876372154f
nhicu <55456980+nhicu@users.noreply.github.com>
2019-11-06 11:54:58 -0500

    um

--- src/sign.js
+++ src/sign.js
@@ -1,13 +1,17 @@
 const readlineSync = require("readline-sync");
-
+const min = Number.MIN_SAFE_INTEGER
+const max = Number.MAX_SAFE_INTEGER
 const number = Number(readlineSync.question("\nEnter a number: "));
 
-if (Number.MAX_SAFE_INTEGER(number))  {
-  console.log("\nPositive.");
-}else if (Number.MIN_SAFE_INTEGER(number)) {
-  console.log("\nNegative.");
-}else if (number=0) {
+if(isNaN(number)) {
+  console.log("\nInvalid.");
+
+}else if (number==0) {
   console.log("\nZero.");
-}else if (Number.NaN(number)) {
-  console.log("\nInvalid.")
+
+}else if (number<0) {
+  console.log("\nNegative.");
+
+}else if (number>0)  {
+  console.log("\nPositive.");
 }
